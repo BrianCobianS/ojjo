948133772619349487cdd1f38cca9df13a78d110
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-26 13:51:43 -0600

    a

--- report.py
+++ report.py
@@ -1,11 +1,11 @@
 def readfile(x):
-    ban =0
     level= ''
     contenido = open(x).read().splitlines()
     for line in contenido:
         line.strip()
-        if '1-1' in line or ban==1:
-            ban=1
-            level = level +line
+        if '1-1' in line:
+            level = line
             print(line)
-readfile('temp.txt')
+    return level
+a=readfile('temp.txt')
+print(a.split('|'))
