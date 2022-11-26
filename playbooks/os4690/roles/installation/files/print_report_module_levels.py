3a44b95ef7e595a8fca3193d4f5413eab411679d
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-26 13:09:09 -0600

    a

--- playbooks/os4690/roles/installation/files/print_report_module_levels.py
+++ playbooks/os4690/roles/installation/files/print_report_module_levels.py
@@ -28,16 +28,16 @@ class ReportModuleLevel:
             this_product_divider_line = linecache.getline(REPORT_FILE, 13+(13*i))
 
             # Print out list of products and status from the report
-            print ("\n" + this_product_name_line[:40])
-            print ("\n" +this_product_status_line[13:80])
-            print ("\n" +this_product_pid_line[13:80])
-            print ("\n" +this_product_cd_line[13:80])
-            print ("Este es el realeseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +this_product_release_line[13:80])
-            print ("\n" +this_product_base_level_line[13:80])
-            print ("\n" +this_product_date_applied_line[13:80])
-            print ("\n" +this_product_ptf_line[13:80])
-            print ("\n" +this_product_emergency_fix_line[13:80])
-            print ("\n" +this_product_divider_line[13:80])
+            # print ("\n" + this_product_name_line[:40])
+            # print ("\n" +this_product_status_line[13:80])
+            # print ("\n" +this_product_pid_line[13:80])
+            # print ("\n" +this_product_cd_line[13:80])
+            # print ("Este es el realeseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +this_product_release_line[13:80])
+            # print ("\n" +this_product_base_level_line[13:80])
+            # print ("\n" +this_product_date_applied_line[13:80])
+            # print ("\n" +this_product_ptf_line[13:80])
+            # print ("\n" +this_product_emergency_fix_line[13:80])
+            # print ("\n" +this_product_divider_line[13:80])
 
             i += 1
 
