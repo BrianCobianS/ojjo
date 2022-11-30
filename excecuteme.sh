89dc4b91854cce31cfd693c6720731d65687f31e
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-30 16:40:44 -0600

    updatete for RPML

--- excecuteme.sh
+++ excecuteme.sh
@@ -1,2 +1,2 @@
-ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml  2>&1 | tee temp.txt
+ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -vv  -i /home/ebossteam/RML/ojjo/Inventories/import_inventory.yml  2>&1 | tee temp.txt
 python3  /home/ebossteam/RML/ojjo/report.py
