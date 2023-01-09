0e5d267bb6c29012b8bc406075d1cee3cef08aba
brian.cobian <brian.cobian@toshibagcs.com>
2023-01-09 15:51:37 -0600

    Change varible names

--- excecuteme.sh
+++ excecuteme.sh
@@ -1,2 +1,2 @@
-ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -v -i /home/ebossteam/RML/ojjo/Inventories/import_inventory.yml 2>&1 | tee  /home/ebossteam/RML/ojjo/temp.txt
+ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -v -i /home/ebossteam/RML/ojjo/Inventories/import_inventory.yml -e 'product=AI'2>&1 | tee  /home/ebossteam/RML/ojjo/temp.txt
 python3  /home/ebossteam/RML/ojjo/report.py
