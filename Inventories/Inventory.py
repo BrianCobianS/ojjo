ce146f9a7752dfd64dcd88d3d4baf432ea0d2a82
brian.cobian <brian.cobian@toshibagcs.com>
2023-01-11 11:42:23 -0600

    Change varible names

--- Inventories/Inventory.py
+++ Inventories/Inventory.py
@@ -3,9 +3,16 @@ import os
 IP=(sys.argv[1])
 USR=(sys.argv[2])
 PASS=(sys.argv[3])
+Product=(sys.argv[4])
 print(IP,USR,PASS)
 print(os.getcwd())
 def controller(host,user,password):
+    contenidoexcec = open("/home/ebossteam/RML/ojjo/template.sh").read().splitlines()
+    contenidoexcec.insert(2,"ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -v -i /home/ebossteam/RML/ojjo/Inventories/import_inventory.yml -e 'product="+Product+"' 2>&1 | tee  /home/ebossteam/RML/ojjo/temp.txt")
+    contenidoexcec.insert(3,"python3  /home/ebossteam/RML/ojjo/report.py")
+    f = open('/home/ebossteam/RML/ojjo/excecuteme.sh', "w")
+    f.writelines("\n".join(contenido))
+    f.close
     #contenido = open("./Inventories/template.txt").read().splitlines()
     contenido = open("/home/ebossteam/RML/ojjo/Inventories/template.txt").read().splitlines()
     contenido.insert(6,"            "+host)
--- excecuteme.sh
+++
@@ -1,2 +0,0 @@
-ansible-playbook  /home/ebossteam/RML/ojjo/playbooks/os4690/Install_Controller.yml -v -i /home/ebossteam/RML/ojjo/Inventories/import_inventory.yml -e 'product=AI' 2>&1 | tee  /home/ebossteam/RML/ojjo/temp.txt
-python3  /home/ebossteam/RML/ojjo/report.py
